5c34ae9d6ffd9b579042f34e63e6f8e44bda68e5
Damon Fernandez <damonfernandez04@gmail.com>
2024-05-25 03:56:43 -0400

    added function to get file content in array form and set up an condition to only run our main block of code if the submit button exists in

--- q1/index.php
+++ q1/index.php
@@ -1,8 +1,30 @@
 <?php 
 
-$submitButton = $_POST[]
 
+if(isset($_POST["submitButton"]) && isset($_FILES["filePicker"])){
+    
+    $textBoxValue = $_POST["textBox"] ?? "";
+    
+    // Checking for an error
+    if($file['error'] == 0){
 
+    $fileContent = getFileContentInArrayForm();
+
+    $numberOfWordsP
+
+}
+
+}
+
+function countNumberOfWordsPerLine(fileContent){
+
+}
+
+function getFileContentInArrayForm(){
+    $file = $_FILES["filePicker"] ?? null;
+    $fileContentStringForm = file_get_contents($file['tmp_name']);
+    $fileContentArrayForm = explode("\n", $fileContentArrayForm)
+}
 
 ?>
 
@@ -11,23 +33,35 @@ $submitButton = $_POST[]
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Assignment One - Question One</title>
 </head>
 <body>
-    <form method="post">
+    <h1>Assignment One - Question One</h1>
+    <form method="post" enctype="multipart/form-data>
         <fieldset>
             <label for="textBox"></label>
-            <input id="textBox"type="text">
+            <input name="textBox" id="textBox"type="text">
         </fieldset>
 
         <fieldset>
             <label for="filePicker"></label>
-            <input id="filePicker" type="file">
+            <input name="filePicker" id="filePicker" type="file">
         </fieldset>
 
-        <button type="submit">Submit</button>
+        <button name="submitButton" type="submit">Submit</button>
     </form>
 
-    <output></output>
+    <output> 
+    
+    <p> Base File Content:
+    <?php if(isset($fileContent)):?>
+    <?php
+        $foreach($currentLine as $fileContent): ?>
+            <?= $currentLine ?>
+  
+    <?php endforeach; ?>
+    <?php endif; ?>
+    </p>
+    </output>
 </body>
 </html>
