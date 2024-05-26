6570fdfcdd07a67dc323730fd1f7a8a7e3dd116b
Damon Fernandez <damonfernandez04@gmail.com>
2024-05-26 18:47:21 -0400

    wrote question class for q2

--- q2/index.php
+++ q2/index.php
@@ -1,22 +1,93 @@
 <?php 
 
 $choiceOne = "choice one";
-$choiceTwo= "choice one";
-$choiceThree = "choice one";
-$choiceFour = "choice one";
+$choiceTwo= "choice two";
+$choiceThree = "choice three";
+$choiceFour = "choice four";
 
 
 function checkForSubmissionButton(){
-    
+
 }
 
 
-class Question{
+class Question {
+    private $choiceOne;
+    private $choiceTwo;
+    private $choiceThree;
+    private $choiceFour;
+    private $correctAnswer;
+    private $questionText;
 
-    Question(){
+    // Constructor
+    function __construct($questionText, $choiceOne, $choiceTwo, $choiceThree, $choiceFour, $correctAnswer) {
+        $this->questionText = $questionText;
+        $this->choiceOne = $choiceOne;
+        $this->choiceTwo = $choiceTwo;
+        $this->choiceThree = $choiceThree;
+        $this->choiceFour = $choiceFour;
+        $this->correctAnswer = $correctAnswer;
+    }
 
+    // Getters
+    public function getQuestionText() {
+        return $this->questionText;
+    }
+
+    public function getChoiceOne() {
+        return $this->choiceOne;
+    }
+
+    public function getChoiceTwo() {
+        return $this->choiceTwo;
+    }
+
+    public function getChoiceThree() {
+        return $this->choiceThree;
+    }
+
+    public function getChoiceFour() {
+        return $this->choiceFour;
+    }
+
+    public function getCorrectAnswer() {
+        return $this->correctAnswer;
+    }
+
+    // Setters
+    public function setQuestionText($questionText) {
+        $this->questionText = $questionText;
+    }
+
+    public function setChoiceOne($choiceOne) {
+        $this->choiceOne = $choiceOne;
+    }
+
+    public function setChoiceTwo($choiceTwo) {
+        $this->choiceTwo = $choiceTwo;
+    }
+
+    public function setChoiceThree($choiceThree) {
+        $this->choiceThree = $choiceThree;
+    }
+
+    public function setChoiceFour($choiceFour) {
+        $this->choiceFour = $choiceFour;
+    }
+
+    public function setCorrectAnswer($correctAnswer) {
+        $this->correctAnswer = $correctAnswer;
     }
 }
+
+$question = new Question(
+    "What is the capital of France?",
+    "Berlin",
+    "Madrid",
+    "Paris",
+    "Rome",
+    "Paris"
+);
 ?>
 
 <!DOCTYPE html>
