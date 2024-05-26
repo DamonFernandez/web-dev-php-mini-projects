ed7a6a14685b4bed0cf2c4f13c2fd29556014a25
Damon Fernandez <damonfernandez04@gmail.com>
2024-05-26 00:43:27 -0400

    wrote starter HTML for q2

---
+++ q2/index.php
@@ -0,0 +1,28 @@
+<?php 
+
+$choiceOne = "choice one";
+$choiceTwo= "choice one";
+$choiceThree = "choice one";
+$choiceFour = "choice one";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Assignment One - Question Two</h1>
+
+    <form action="">
+    <button><?= $choiceOne?></button>
+    <button><?= $choiceTwo?></button>
+    <button><?= $choiceThree?></button>
+    <button><?= $choiceFour?></button>
+    </form>
+
+</body>
+</html>
