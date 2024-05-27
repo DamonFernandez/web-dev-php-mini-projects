ddb84a92a928a10772128711e19237c25440c8aa
Blitzcranq <vrajchauhan2110@gmail.com>
2024-05-27 16:27:04 -0400

    fixed a bug to handle empty lines in the file (q1)

--- q1/index.php
+++ q1/index.php
@@ -1,50 +1,54 @@
-<?php 
-
-
-
-   
-    $fileContent = getFileContentInArrayForm() ?? [];
-
-
-    
+<?php
 
 
 
 
-if(isset($_POST["submitButton"]) && isset($_FILES["filePicker"])){
-    
+$fileContent = getFileContentInArrayForm() ?? [];
+
+
+
+
+
+
+
+if (isset($_POST["submitButton"]) && isset($_FILES["filePicker"])) {
+
     $textBoxValue = $_POST["textBox"] ?? "";
-
-}
-
-else{
+} else {
     $fileContent = [];
 }
 
 
-function getFileContentInArrayForm() {
+function getFileContentInArrayForm()
+{
     $file = $_FILES["filePicker"] ?? null;
     if ($file !== null && isset($file['tmp_name'])) {
         $fileContentStringForm = file_get_contents($file['tmp_name']);
         $fileContentArrayForm = explode("\n", $fileContentStringForm);
+        $fileContentArrayForm =  array_filter($fileContentArrayForm, function ($line) {
+            return trim($line) !== "";
+        });
+        var_dump($fileContentArrayForm);
         return $fileContentArrayForm;
     }
     return []; // Return an empty array if the file isn't set or theres no tmp_name
 }
 
-function countNumberOfWordsPerLine($fileContent){
+function countNumberOfWordsPerLine($fileContent)
+{
     $lineCounter = 1;
-    foreach($fileContent as $currentLine){
+    foreach ($fileContent as $currentLine) {
         $amountOfWordsInCurrentLine = count(explode(" ", $currentLine));
         echo "Amount of words in line $lineCounter: $amountOfWordsInCurrentLine<br>";
         $lineCounter++;
     }
 }
 
-function countNumberOfACharactersPerLine($fileContent){
+function countNumberOfACharactersPerLine($fileContent)
+{
     $lineCounter = 1;
 
-    foreach($fileContent as $currentLine){
+    foreach ($fileContent as $currentLine) {
         $numberOfACharactersCounter = 0;
 
         $numberOfACharactersCounter += substr_count($currentLine, "a");
@@ -58,23 +62,23 @@ function countNumberOfACharactersPerLine($fileContent){
 
 
 // for simplicity, we'll consider them to be characters with an ascii value between 32-47) You could certainly accomplish this with regular expressions, but it can also be done fairy easily with basic php functions.
-function countNumberOfCommonPunctuationCharactersPerLine($fileContent){
-
+function countNumberOfCommonPunctuationCharactersPerLine($fileContent)
+{
 }
 
 
 // irrespective of case).
-function sortStringDescAlphabeticalPerLine($fileContent){
-
+function sortStringDescAlphabeticalPerLine($fileContent)
+{
 }
 // The middle-third characters of the string, if the string isn't evenly divisible by three, you should take the lower value. Example: if the string is 15 characters long, you would output characters 6-10, if it's 16 or 17 characters, you would still output character 6-10
-function printMiddleThirdCharacterPerLine($fileContent){
-
+function printMiddleThirdCharacterPerLine($fileContent)
+{
 }
 
 
-function filterStrings($fileContent){
-
+function filterStrings($fileContent)
+{
 }
 
 
@@ -83,17 +87,19 @@ function filterStrings($fileContent){
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Assignment One - Question One</title>
 </head>
+
 <body>
     <h1>Assignment One - Question One</h1>
     <form method="post" enctype="multipart/form-data">
         <fieldset>
             <label for="textBox">Search:</label>
-            <input name="textBox" id="textBox"type="text">
+            <input name="textBox" id="textBox" type="text">
         </fieldset>
 
         <fieldset>
@@ -104,27 +110,27 @@ function filterStrings($fileContent){
         <button name="submitButton" type="submit">Submit</button>
     </form>
 
-  
-    
+
+
     <h2> Base File Content:</h2>
-    <output> 
-    <?php if(isset($fileContent)):?>
-    <?php
-        foreach($fileContent as $currentLine): ?>
-            <?= $currentLine ?>
-  
-    <?php endforeach; ?>
-    <?php endif; ?>
+    <output>
+        <?php if (isset($fileContent)) : ?>
+            <?php
+            foreach ($fileContent as $currentLine) : ?>
+                <?= $currentLine ?><br>
+            <?php endforeach; ?>
+        <?php endif; ?>
     </output>
 
 
     <h2> Number of words per line </h2>
-    <output> <?= countNumberOfWordsPerLine($fileContent)?> </output>
-  
+    <output> <?= countNumberOfWordsPerLine($fileContent) ?> </output>
+
 
     <h2>Number of A's and a's per line</h2>
-    <output> <?= countNumberOfACharactersPerLine($fileContent)?></output>
+    <output> <?= countNumberOfACharactersPerLine($fileContent) ?></output>
 
 
 </body>
+
 </html>
