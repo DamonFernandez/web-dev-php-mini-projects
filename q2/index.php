dd18fea29b931e4c04cd1fba3d130d4a1722c8e8
Damon Fernandez <damonfernandez04@gmail.com>
2024-06-01 23:37:09 -0400

    q2: add main section and fieldsets to html

--- q2/index.php
+++ q2/index.php
@@ -52,22 +52,35 @@ if (isset($_POST["submit"])) {
 </head>
 
 <body>
-    <h1>Guess the anime: </h1>
-    <span class="question"> Anime: <?= $current_question + 1 . "/" . count($questions)  . " $questions[$current_question]" ?> </span>
+    <main>
+        <h1>Guess the anime: </h1>
+        <span class="question"> Anime: <?= $current_question + 1 . "/" . count($questions)  . " $questions[$current_question]" ?> </span>
+        <form method="post">
+        <fieldset>
+    <input type="radio" name="choice" value="0" id="0">
+    <label for="0"><?= $questions[$current_question]->getChoice(0) ?></label>
+</fieldset>
 
-    <form method="post">
-        <input type="radio" name="choice" value="0" id="0">
-        <label for="choiceOne"><?= $questions[$current_question]->getChoice(0) ?></label>
-        <input type="radio" name="choice" value="1" id="1">
-        <label for="choiceTwo"><?= $questions[$current_question]->getChoice(1) ?></label>
-        <input type="radio" name="choice" value="2" id="2">
-        <label for="choiceThree"><?= $questions[$current_question]->getChoice(2) ?></label>
-        <input type="radio" name="choice" value="3" id="3">
-        <label for="choiceFour"><?= $questions[$current_question]->getChoice(3) ?></label>
-        <button type="submit" name="submit">Submit</button>
-        <span class="red <?= !isset($errors['choice']) ? 'hidden' : ''  ?>"> you didnt try guessing!</span>
-        <button name="restart">Restart </button>
-    </form>
+<fieldset>
+    <input type="radio" name="choice" value="1" id="1">
+    <label for="1"><?= $questions[$current_question]->getChoice(1) ?></label>
+</fieldset>
+
+<fieldset>
+    <input type="radio" name="choice" value="2" id="2">
+    <label for="2"><?= $questions[$current_question]->getChoice(2) ?></label>
+</fieldset>
+
+<fieldset>
+    <input type="radio" name="choice" value="3" id="3">
+    <label for="3"><?= $questions[$current_question]->getChoice(3) ?></label>
+</fieldset>
+
+            <button type="submit" name="submit">Submit</button>
+            <span class="red <?= !isset($errors['choice']) ? 'hidden' : ''  ?>"> you didnt try guessing!</span>
+            <button name="restart">Restart </button>
+        </form>
+    </main>
 
 </body>
 
