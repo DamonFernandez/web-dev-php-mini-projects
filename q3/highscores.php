ad735cff93d174be7731eaf8bcecc1d353acbd40
Damon Fernandez <damonfernandez04@gmail.com>
2024-06-04 00:18:34 -0400

    Q3: Add comments for high scores page

--- q3/highscores.php
+++ q3/highscores.php
@@ -1,20 +1,30 @@
 <?php
 
+/**
+ * Function to render a list of scores as HTML list items
+ */
 function renderScores($setOfScores)
 {
+    // Iterate over each row in the set of scores
     foreach ($setOfScores as $currRow) {
+        // Output the current row as an HTML list item
         echo "<li> {$currRow['Name']} - Score: {$currRow['Score']} </li>";
     }
 }
 
+// Include the library file that contains the database connection function
 include "./includes/library.php";
+
+// Connect to the database
 $pdo = connectdb();
+
+// Prepare and execute the query to fetch the top 20 scores from the database
 $query = $pdo->query(
     "SELECT * FROM rock_paper_spock_scores ORDER BY score DESC LIMIT 20 ;"
 );
 
+// Fetch all the results from the query
 $queryResults = $query->fetchAll();
-var_dump($queryResults);
 
 
 ?>
@@ -28,9 +38,12 @@ var_dump($queryResults);
 </head>
 
 <body>
+    <!-- Include the header file -->
     <?php include_once "./includes/header.php" ?>
+
     <h1>High Scores</h1>
     <ol>
+        <!-- Render the scores as an ordered list -->
         <?= renderScores($queryResults) ?>
     </ol>
 
