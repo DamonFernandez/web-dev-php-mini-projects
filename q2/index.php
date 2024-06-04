fc5f05e0e36d9ccde76af19567691f9b3593a405
Blitzcranq <vrajchauhan2110@gmail.com>
2024-06-04 18:12:38 -0400

    fixed a bug q-2. all working now !

--- q2/index.php
+++ q2/index.php
@@ -6,55 +6,11 @@
  */
 
 // Include the Question class
+require_once "./questions.php";
 
-$questionOne = new Question( // Create a new Question object for the first question
-    "Which anime features a boy named Tanjiro Kamado who becomes a demon slayer after his family is slaughtered and his sister is turned into a demon?",
-    "Demon Slayer",
-    "Attack on Titan",
-    "Blue Exorcist",
-    "Naruto",
-    0
-);
-$questionTwo = new Question(
-    "In which anime does a young alchemist named Edward Elric use alchemy in a quest to find the Philosopher's Stone to restore his and his brother's bodies?",
-    "Fullmetal Alchemist: Brotherhood",
-    "My Hero Academia",
-    "Sword Art Online",
-    "Black Clover",
-    0
-);
-$questionThree = new Question(
-    "Which anime follows a group of pirates led by Monkey D. Luffy in their search for the ultimate treasure, the One Piece?",
-    "One Piece",
-    "Fairy Tail",
-    "Bleach",
-    "Hunter x Hunter",
-    0
-);
-$questionFour = new Question(
-    "In which anime does a boy named Gon Freecss become a Hunter to find his missing father and uncover the secrets of the Hunter Association?",
-    "Naruto",
-    "One Piece",
-    "Hunter x Hunter",
-    "Dragon Ball Z",
-    2
-);
-$questionFive = new Question(
-    "Which anime is set in a world where 80% of the population has superpowers, and follows a boy named Izuku Midoriya who dreams of becoming the greatest hero?",
-    "My Hero Academia",
-    "One Punch Man",
-    "Blue Exorcist",
-    "Attack on Titan",
-    0
-);
-
-$questions = [$questionOne, $questionTwo, $questionThree, $questionFour, $questionFive]; // Create an array of Question objects
 
 // Start the session
 session_start();
-
-// Include the questions file
-require_once "./questions.php";
 
 // Restart the quiz if the restart button is clicked
 if (isset($_POST['restart'])) {
